b54a5282817359bbe7435ed160ef1fe8607e1baf
ricpar123 <ricpar123@gmail.com>
2024-11-06 08:02:58 -0300

    update 5

--- js/qrcode.js
+++ js/qrcode.js
@@ -16,7 +16,7 @@ domReady(function () {
 
     // If found you qr code
     function onScanSuccess(decodeText, decodeResult) {
-        result = + decodeText, decodeResult;
+        result = decodeText;
         alert('resultado:', result);
         alert("You Qr is : " + decodeText, decodeResult);
     }
